a2663fbac4bca971dea9444149dbfdffeeec52e8
Mitchell Zubich <mitchellzubich@gmail.com>
2021-08-03 15:54:27 -0400

    adding cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 var app = express();
+
+app.use(cors);
 
 app.use(function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
