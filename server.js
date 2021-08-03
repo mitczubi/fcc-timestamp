65cd98d71f8d47885d67ac04780b3cc8591155ec
Mitchell Zubich <mitchellzubich@gmail.com>
2021-08-03 16:18:06 -0400

    removing cors

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 
 var app = express();
 const port = process.env.PORT || 3000
-
-app.use(cors);
 
 app.use(function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
